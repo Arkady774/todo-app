616fec8f633e013de8a76f27d9c06f23894eaac4
arkady <antistov174@mail.ru>
2025-12-13 17:37:09 +0500

    Подключил Lombok, отредактировал Pom.xml, упростил локику TodoItem.

--- src/main/java/com/example/todo_app/model/TodoItem.java
+++ src/main/java/com/example/todo_app/model/TodoItem.java
@@ -1,9 +1,17 @@
 package com.example.todo_app.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TodoItem {
     private Long id;
     private String title;
@@ -12,30 +20,4 @@ public class TodoItem {
 
     @JsonFormat(pattern = "dd-MM-yyyy")
     private LocalDate date;
-
-    public TodoItem() {}
-
-    public TodoItem(Long id, String title, String description, LocalDate date, boolean completed) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.date = date;
-        this.completed = completed;
-    }
-
-    // Геттеры и сеттеры
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
-
-    public LocalDate getDate() { return date; }
-    public void setDate(LocalDate date) { this.date = date; }
-
-    public boolean isCompleted() { return completed; }
-    public void setCompleted(boolean completed) { this.completed = completed; }
 }
